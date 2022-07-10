ddc2739386627a8fe5f27e05ad8b8c66a76b8c7f
박희준 <107246410+heejuneee@users.noreply.github.com>
2022-07-10 20:55:20 +0900

    Create Level(2)target.cpp

---
+++ Level(2)target/Level(2)target.cpp
@@ -0,0 +1,40 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool promising(int depth, const vector<int> &numbers, int search, int target) {
+    int lastSum = 0;
+    for (int i = depth; i < numbers.size(); i++) {
+        lastSum += numbers[i];
+    }
+
+    if (search + lastSum < target || search - lastSum > target) {
+        return false;
+    }
+    else return true;
+}
+
+void searchNum(int &answer, int depth, const vector<int> &numbers, int search, int target) {
+    if (promising(depth, numbers, search, target)) {
+        if (depth == numbers.size()) {
+            answer++;
+            return;
+        }
+
+        search += numbers[depth];
+        searchNum(answer, depth + 1, numbers, search, target);
+
+        search -= (2 * numbers[depth]);
+        searchNum(answer, depth + 1, numbers, search, target);
+    }
+}
+
+int solution(vector<int> numbers, int target) {
+
+    int answer = 0;
+
+    searchNum(answer, 0, numbers, 0, target);
+
+    return answer;
+}

8ebab11b1ce613b86574027d2ea2ee7140fd0c3b
박희준 <107246410+heejuneee@users.noreply.github.com>
2022-07-11 23:44:51 +0900

    Update printer.cpp

--- Level(2)printer/printer.cpp
+++ Level(2)printer/printer.cpp
@@ -10,3 +10,13 @@ int solution(vector<int> priorities, int location) {
     queue <pair<int,int>>q;
     priority_queue <int>pq;
     for(int i = 0; i < priorities.size(); i++){pq.push(priorities[i]); q.push(make_pair(i, priorities[i]));}
+
+   pair<int,int> tmp;
+    while(!q.empty()){
+        if(q.front().second == pq.top() && q.front().first == location){break;}
+        else if(q.front().second == pq.top()) {q.pop(); pq.pop();answer++;}
+        else {tmp = q.front(); q.pop(); q.push(tmp);}
+    }
+
+    return answer + 1;
+}

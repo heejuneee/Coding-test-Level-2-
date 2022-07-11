cffae0c93491df11c054833730ab6943eae62c8c
박희준 <107246410+heejuneee@users.noreply.github.com>
2022-07-11 23:49:01 +0900

    Update printer.js

--- Level(2)printer/printer.js
+++ Level(2)printer/printer.js
@@ -6,3 +6,16 @@ function solution(priorities, location) {
     });
 
     var queue = [];
+
+     while(arr.length > 0) {
+        var firstEle = arr.shift();
+        var hasHighPriority = arr.some(ele => ele.priority > firstEle.priority);
+        if (hasHighPriority) {
+            arr.push(firstEle);
+        } else {
+            queue.push(firstEle);
+        }
+    }
+
+    return queue.findIndex(queueEle => queueEle.index === location) + 1;
+}

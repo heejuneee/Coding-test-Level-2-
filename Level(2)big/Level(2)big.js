5893f927adb81d72c15da783429e171fbb30482b
박희준 <107246410+heejuneee@users.noreply.github.com>
2022-07-12 23:50:55 +0900

    Update Level(2)big.js

--- Level(2)big/Level(2)big.js
+++ Level(2)big/Level(2)big.js
@@ -8,3 +8,12 @@ const solution = (number, k) => {
             stack.push(item)
             continue;
         }
+// stack에 쌓인 최근 값이 들어와야할 값보다 크거나 같을때까지 꺼낸다.
+        while (stack[stack.length - 1] < item) {
+            stack.pop()
+            count++
+            // 만약 숫자를 빼야할만큼 뺐다면 완성된 값을 반환한다.
+            if (count === k) return stack.join("") + number.slice(i, number.length)
+            // 스택이 비어있으면 루프를 멈추고 스택에 아이템을 추가한다.
+            if (stack.length === 0) break;
+        }

95df38c0e1cf8f9ab83c65e856a2cb558ad42291
박희준 <107246410+heejuneee@users.noreply.github.com>
2022-07-14 23:20:43 +0900

    Update Level(2)boat.js

--- Level(2)boat/Level(2)boat.js
+++ Level(2)boat/Level(2)boat.js
@@ -30,3 +30,10 @@ const solution = (pplArr, limit) => {
       acc.push([person]);
       return acc;
     }
+ const len = acc.length;
+    // 자기 바로 앞의 무거운 사람과 한 배 탈 수 있으면 둘이 합승시키고 출발~
+    if (acc[len - 1] && acc[len - 1][0] + person <= limit) {
+      acc.pop();
+      acc.unshift([limit, limit]);
+      return acc;
+    }

c16c75d0b0e4c18e163eeb278b87bf23d888fa51
박희준 <107246410+heejuneee@users.noreply.github.com>
2022-07-15 22:50:11 +0900

    Create Level(2)jump.js

---
+++ Level(2)jump/Level(2)jump.js
@@ -0,0 +1,12 @@
+let memo =  []
+function init(){
+  let s1 = 0
+  let s2 = 1
+  for(let i = 1; i < 1500; i++){
+    s1 = s1 + s2
+    s2 = s1 + s2
+    memo.push(s1)
+    memo.push(s2)
+  }
+}
+init()

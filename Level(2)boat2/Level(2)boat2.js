5a56b5407717456b3a7539e8f9b427eb4d7c6a7c
박희준 <107246410+heejuneee@users.noreply.github.com>
2022-08-08 11:11:27 +0900

    Update Level(2)boat2.js

--- Level(2)boat2/Level(2)boat2.js
+++ Level(2)boat2/Level(2)boat2.js
@@ -1,1 +1,16 @@
-.
+function countingSort(arr, min, max) {
+  const countHash = arr
+    .reduce((acc, num) => {
+      acc[num] += 1;
+      return acc;
+    }, new Array(max + 1).fill(0))
+    .reduce((acc, num, idx) => {
+      if (idx === 0) return [0];
+      acc.push(num + acc[idx - 1]);
+      return acc;
+    }, []);
+    const result = arr.reduce((acc,num)) =>{
+        
+    }
+    
+    

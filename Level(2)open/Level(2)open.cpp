7c6d8208fa7bfa48c3f6756af860617471bcd6cf
박희준 <107246410+heejuneee@users.noreply.github.com>
2022-08-11 10:34:42 +0900

    Update Level(2)open.cpp

--- Level(2)open/Level(2)open.cpp
+++ Level(2)open/Level(2)open.cpp
@@ -7,32 +7,32 @@
 
 using namespace std;
 
-vector<string> solution(vector<string> record) {
-    vector<string> answer;
-    vector<string>::iterator it;
-    vector<pair<string,string> > vv;
-    map<string, string> m;
-    string buf1, buf2, buf3;
-    for(it=record.begin(); it!=record.end(); it++){
-        stringstream streams;
-        streams.str(*it);
-        streams >> buf1;
-        if(buf1 == "Enter"){
-            streams >> buf2;
-            streams >> buf3;
-            m.erase(buf2);
-            m.insert(make_pair(buf2,buf3));
-            vv.push_back(make_pair(buf1,buf2));
-        }else if(buf1 == "Leave"){
-            streams >> buf2;
-            vv.push_back(make_pair(buf1,buf2));
-        }else if(buf1 == "Change"){
-            streams >> buf2;
-            streams >> buf3;
-            m.erase(buf2);
-            m.insert(make_pair(buf2,buf3));
-        }
-    }
+// vector<string> solution(vector<string> record) {
+//     vector<string> answer;
+//     vector<string>::iterator it;
+//     vector<pair<string,string> > vv;
+//     map<string, string> m;
+//     string buf1, buf2, buf3;
+//     for(it=record.begin(); it!=record.end(); it++){
+//         stringstream streams;
+//         streams.str(*it);
+//         streams >> buf1;
+//         if(buf1 == "Enter"){
+//             streams >> buf2;
+//             streams >> buf3;
+//             m.erase(buf2);
+//             m.insert(make_pair(buf2,buf3));
+//             vv.push_back(make_pair(buf1,buf2));
+//         }else if(buf1 == "Leave"){
+//             streams >> buf2;
+//             vv.push_back(make_pair(buf1,buf2));
+//         }else if(buf1 == "Change"){
+//             streams >> buf2;
+//             streams >> buf3;
+//             m.erase(buf2);
+//             m.insert(make_pair(buf2,buf3));
+//         }
+//     }
     vector<pair<string,string> >::iterator it2;
     for(it2=vv.begin();it2!=vv.end();it2++){
         string result = m.find(it2->second)->second;

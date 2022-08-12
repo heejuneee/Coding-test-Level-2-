f322c2d0e6e4e5404db52b959955ba5157a31a57
박희준 <107246410+heejuneee@users.noreply.github.com>
2022-08-12 09:46:11 +0900

    Update printer.cpp

--- Level(2)printer/printer.cpp
+++ Level(2)printer/printer.cpp
@@ -1,22 +1,22 @@
-#include <string>
-#include <queue>
-#include <iostream>
-#include <vector>
+// #include <string>
+// #include <queue>
+// #include <iostream>
+// #include <vector>
 
-using namespace std;
+// using namespace std;
 
-int solution(vector<int> priorities, int location) {
-    int answer = 0;
-    queue <pair<int,int>>q;
-    priority_queue <int>pq;
-    for(int i = 0; i < priorities.size(); i++){pq.push(priorities[i]); q.push(make_pair(i, priorities[i]));}
+// int solution(vector<int> priorities, int location) {
+//     int answer = 0;
+//     queue <pair<int,int>>q;
+//     priority_queue <int>pq;
+//     for(int i = 0; i < priorities.size(); i++){pq.push(priorities[i]); q.push(make_pair(i, priorities[i]));}
 
-   pair<int,int> tmp;
-    while(!q.empty()){
-        if(q.front().second == pq.top() && q.front().first == location){break;}
-        else if(q.front().second == pq.top()) {q.pop(); pq.pop();answer++;}
-        else {tmp = q.front(); q.pop(); q.push(tmp);}
-    }
+//    pair<int,int> tmp;
+//     while(!q.empty()){
+//         if(q.front().second == pq.top() && q.front().first == location){break;}
+//         else if(q.front().second == pq.top()) {q.pop(); pq.pop();answer++;}
+//         else {tmp = q.front(); q.pop(); q.push(tmp);}
+//     }
 
-    return answer + 1;
-}
+//     return answer + 1;
+// }

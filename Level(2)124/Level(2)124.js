d5cb7d017499ffc4c488c41c5571fa55e30e524a
박희준 <107246410+heejuneee@users.noreply.github.com>
2022-08-23 08:34:45 +0900

    Update Level(2)124.js

--- Level(2)124/Level(2)124.js
+++ Level(2)124/Level(2)124.js
@@ -4,20 +4,20 @@ const div = (a, b) => parseInt(a / b);
 
 // test --------------------
 
-function test() {}
+// function test() {}
 
-test('simple', () => {
-  expect(solution(1)).toBe('1');
-  expect(solution(2)).toBe('2');
-  expect(solution(3)).toBe('4');
-  expect(solution(4)).toBe('11');
-  expect(solution(5)).toBe('12');
-  expect(solution(6)).toBe('14');
-  expect(solution(7)).toBe('21');
-  expect(solution(8)).toBe('22');
-  expect(solution(9)).toBe('24');
-  expect(solution(10)).toBe('41');
-  expect(solution(11)).toBe('42');
-  expect(solution(12)).toBe('44');
-  expect(solution(13)).toBe('111');
-});
+// test('simple', () => {
+//   expect(solution(1)).toBe('1');
+//   expect(solution(2)).toBe('2');
+//   expect(solution(3)).toBe('4');
+//   expect(solution(4)).toBe('11');
+//   expect(solution(5)).toBe('12');
+//   expect(solution(6)).toBe('14');
+//   expect(solution(7)).toBe('21');
+//   expect(solution(8)).toBe('22');
+//   expect(solution(9)).toBe('24');
+//   expect(solution(10)).toBe('41');
+//   expect(solution(11)).toBe('42');
+//   expect(solution(12)).toBe('44');
+//   expect(solution(13)).toBe('111');
+// });

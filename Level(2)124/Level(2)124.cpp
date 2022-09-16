b97bcd32dc0a7ac141c46efd92a41cd30cbbeaeb
박희준 <107246410+heejuneee@users.noreply.github.com>
2022-09-16 09:00:26 +0900

    Update Level(2)124.cpp

--- Level(2)124/Level(2)124.cpp
+++ Level(2)124/Level(2)124.cpp
@@ -15,31 +15,31 @@ string change124(int no)
 
     int na = 1;
 
-    while (1)
-    {
+//     while (1)
+//     {
 
-        if (temp == 0) mok--;
-        if (mok == 0) break;
-        na *= 3;
-        temp = mok % 3;
-        mok /= 3;
+//         if (temp == 0) mok--;
+//         if (mok == 0) break;
+//         na *= 3;
+//         temp = mok % 3;
+//         mok /= 3;
 
-        if (temp == 0) answer.push_back('4');
-        if (temp == 1) answer.push_back('1');
-        if (temp == 2) answer.push_back('2');
-    }
+//         if (temp == 0) answer.push_back('4');
+//         if (temp == 1) answer.push_back('1');
+//         if (temp == 2) answer.push_back('2');
+//     }
 
-    for (int i = 0; i < answer.size(); i++)
-    {
-        answer2.push_back(answer[answer.size() - i - 1]);
-    }
+//     for (int i = 0; i < answer.size(); i++)
+//     {
+//         answer2.push_back(answer[answer.size() - i - 1]);
+//     }
 
-    return answer2;
-}
-int main()
-{
-    int testNo = 33;
-    string testAnswer = change124(testNo);
+//     return answer2;
+// }
+// int main()
+// {
+//     int testNo = 33;
+//     string testAnswer = change124(testNo);
 
-    cout << testAnswer;
-}
+//     cout << testAnswer;
+// }
